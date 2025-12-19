7f7a8f797d8485dc0eca1baff58d5ecc9908aea3
ramajalab63 <ramajalab63@gmail.com>
2025-12-19 20:21:47 +0300

    Add cin.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int age=26;
 float monthlysalary=5.500;
 char gender='f';
 bool is_maried=true;
+char your_work;
+int your_birth_month;
 
 
 int main() {
@@ -15,6 +17,12 @@ int main() {
     cout << "yearlysalary is " << monthlysalary*12  << endl;
     cout <<"gender is " << gender << endl;
     cout <<"is maried? " << is_maried << endl; 
+    cout <<"please enter your work" << endl;
+    cin >> your_work;
+    cout << "please enter your birthmonth" << endl;
+    cin >> your_birth_month;
+    cout << "your work is " << your_work << endl;
+    cout << "your birth month is "  << your_birth_month << endl;
     cout << "*\t\t*\n";
     cout << "*        *\n" ;
     cout << "\a";
