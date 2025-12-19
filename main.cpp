0a73288369015542ee131d17b6ed804de7621c20
ramajalab63 <ramajalab63@gmail.com>
2025-12-19 18:39:16 +0300

    Add variables.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
+string name="Rama";
+int age=26;
+float monthlysalary=5.500;
+char gender='f';
+bool is_maried=true;
+
 
 int main() {
     cout << "HelloWorld!\n" ;
+    cout << "name is " << name << "\n";
+    cout << "age is "  <<age << endl;
+    cout << "monthlysalary is " <<monthlysalary << endl;
+    cout << "yearlysalary is " << monthlysalary*12  << endl;
+    cout <<"gender is " << gender << endl;
+    cout <<"is maried? " << is_maried << endl; 
     cout << "*\t\t*\n";
     cout << "*        *\n" ;
     cout << "\a";
