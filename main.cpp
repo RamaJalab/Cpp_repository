7a1f07fe418eabc39db54883f121eef9f7111237
ramajalab63 <ramajalab63@gmail.com>
2025-12-26 00:34:51 +0300

    Arithmetic Operators.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ char gender='f';
 bool is_maried=true;
 string your_work;
 int your_birth_month;
-
+int a=25,b=5;
 
 int main() {
+    cout << "a+b= " << a+b << endl;
+    cout << "a-b= " << a-b << endl;
+    cout << "a*b= " << a*b << endl;
+    cout << "a/b= " << a/b << endl;
     cout << "size of int " << sizeof(int) << endl;
     cout << " INT Range : (" << INT_MIN <<"," << INT_MAX <<")\n";
     cout << "HelloWorld!\n" ;
@@ -19,11 +23,6 @@ int main() {
     cout << "yearlysalary is " << monthlysalary*12  << endl;
     cout <<"gender is " << gender << endl;
     cout <<"is maried? " << is_maried << endl; 
-    cout <<"please enter your work" << endl;
-    cin >> your_work;
-    cout << "please enter your birthmonth" << endl;
-    cin >> your_birth_month;
-    cout << "your work is " << your_work << endl;
     cout << "your birth month is "  << your_birth_month << endl;
     
 
